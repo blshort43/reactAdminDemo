08fe32150ea1556c4561038d855d4dcb803a8396
blshort43 <blshort43@gmail.com>
2018-12-03 17:40:56 -0600

    added dashboard page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import { UserList } from "./users.js";
 import { PostList, PostEdit, PostCreate } from "./posts.js";
 import PostIcon from "@material-ui/icons/Book";
 import UserIcon from "@material-ui/icons/Group";
+import Dashboard from "./Dashboard.js";
 
 const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com");
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
     <Resource
       name="posts"
       list={PostList}
