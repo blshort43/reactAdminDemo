c02edc689e74fa2deef5de7929f3e57eaef24cfe
blshort43 <blshort43@gmail.com>
2018-12-03 17:31:02 -0600

    added icons to Posts and Users

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import jsonServerProvider from "ra-data-json-server";
 import { Admin, Resource } from "react-admin";
 import { UserList } from "./users.js";
 import { PostList, PostEdit, PostCreate } from "./posts.js";
+import PostIcon from "@material-ui/icons/Book";
+import UserIcon from "@material-ui/icons/Group";
 
 const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com");
 
@@ -14,7 +16,14 @@ const App = () => (
       edit={PostEdit}
       create={PostCreate}
     />
-    <Resource name="users" list={UserList} />
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+      icon={PostIcon}
+    />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 );
 
