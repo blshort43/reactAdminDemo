038558d79ee38aa49e658127e0c70008fa7f608b
blshort43 <blshort43@gmail.com>
2018-12-04 08:21:24 -0600

    added login page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import { PostList, PostEdit, PostCreate } from "./posts.js";
 import PostIcon from "@material-ui/icons/Book";
 import UserIcon from "@material-ui/icons/Group";
 import Dashboard from "./Dashboard.js";
+import authProvider from "./authProvider";
 
 const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com");
 
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+  <Admin
+    dashboard={Dashboard}
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+  >
     <Resource
       name="posts"
       list={PostList}
